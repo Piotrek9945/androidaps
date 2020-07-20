d0ca9cdf0d51adc8f1ca7ac745351527fe146d55
Piotr Babiuch <piotr.babiuch@core3.pl>
2020-07-20 02:06:48 +0200

    pomniejszanie WBT
    aaa
    
    
    aaa

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/dialogs/TbrDialog.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/dialogs/TbrDialog.java
@@ -35,16 +35,16 @@ public class TbrDialog extends DialogFragment implements OnClickListener, Compou
 
 
     private final int DURATION_1 = 12;
-    private final int TEMP_TARGET_1 = 110;
+    public final int TEMP_TARGET_1 = 110;
     public static final int TBR_PERCENTAGE_1 = 80;
 
 
     private final int DURATION_2 = 24;
-    private final int TEMP_TARGET_2 = 120;
+    public final int TEMP_TARGET_2 = 120;
     public static final int TBR_PERCENTAGE_2 = 50;
 
     private final int DURATION_3 = 48;
-    private final int TEMP_TARGET_3 = 130;
+    public final int TEMP_TARGET_3 = 130;
     public static final int TBR_PERCENTAGE_3 = 20;
 
     private RadioGroup tbrRadioGroup;
